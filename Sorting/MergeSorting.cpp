085fdca7c99498cbf9e9679b42b0ea3b00d85675
Seshu <123055965+seshusai309@users.noreply.github.com>
2025-01-11 18:18:16 +0530

    Update MergeSorting.cpp

--- Sorting/MergeSorting.cpp
+++ Sorting/MergeSorting.cpp
@@ -1,72 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void merge(int arr[], int left, int mid, int right)
-{
-    int i=left;
+void merge(int left, int mid, int right, int arr[]){
+    int i = left;
     int j = mid+1;
-    int k=0;
-    int n = right-left+1;
+    int k = 0;
+    int n = (right-left+1);
     int new_arr[n];
     
-    while(i<=mid and j<=right)
-    {
-        if(arr[i]<arr[j])
-        {
+    while(i<=mid && j<=right){
+        if (arr[i] < arr[j]){
             new_arr[k]=arr[i];
+            k++;
             i++;
-            k++;
-        }
-        else
-        {
+        }else{
             new_arr[k]=arr[j];
-            j++;
             k++;
+            j++;
         }
     }
-    while(i<=mid)
-    {
-        new_arr[k]=arr[i];
-        i++;
-        k++;
-    }
-    while(j<=right)
-    {
-        new_arr[k]=arr[j];
-        j++;
-        k++;
-    }
-    for(int c=0;c<=n-1;c++)
-    {
-        arr[left+c]=new_arr[c];
-    }
-}
-void merge_sort(int arr[], int left, int right)
-{
-    if(left==right)
-    {
-        return;
-    }
-    int mid = (left+right)/2;
-    merge_sort(arr, left, mid);
-    merge_sort(arr, mid+1,right);
     
-    merge(arr,left,mid,right);
+    while(j<=right){
+        new_arr[k]=arr[j];
+        k++;
+        j++;
+    }
+    
+    while(i<=mid){
+        new_arr[k]=arr[i];
+        k++;
+        i++;
+    }
+    
+    for (int c=0; c<n; c++)  arr[left+c] = new_arr[c];
 }
 
-int main()
-{
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    merge_sort(arr,0,n-1);
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    return 0;
+void merge_sort(int left, int right, int arr[]){
+    
+    if (left == right) return;
+    
+    int mid = (left+right) / 2;
+    merge_sort(left, mid, arr);
+    merge_sort(mid+1, right, arr);
+    
+    merge(left,mid,right,arr);
+}
+
+int main() {
+  int n;
+  cin>>n;
+  int arr[n];
+  for (int i=0; i<n; i++) cin>>arr[i];
+  merge_sort(0,n-1,arr);
+  for (auto k: arr) {
+      cout << k << " ";
+  }
+  return 0;
 }
